43156e80a606a37ec3a1b04bcbe8944516a44bc7
Wellinton Rafael Gudilunas <wellintongudilunas788@gmail.com>
2023-09-28 19:30:57 -0300

    feat: add stopwatch and timer

--- app/src/main/java/com/example/stopwatchandtimer/StopWatch.kt
+++ app/src/main/java/com/example/stopwatchandtimer/StopWatch.kt
@@ -1,21 +1,80 @@
 package com.example.stopwatchandtimer
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class StopWatch : AppCompatActivity() {
+    private lateinit var chronometer: TextView
+    private lateinit var startButton: Button
+    private lateinit var resetButton: Button
+    private var isRunning = false
+    private var elapsedTime: Long = 0L
+    private var handler: Handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stop_watch)
 
         val btnGoToTimer = findViewById<Button>(R.id.btnGoToTimer);
-        btnGoToTimer.setOnClickListener{
+        btnGoToTimer.setOnClickListener {
             val timer = Intent(this@StopWatch, Timer::class.java)
             startActivity(timer)
         }
+
+        chronometer = findViewById(R.id.timerTextView)
+        startButton = findViewById(R.id.btnStart)
+        resetButton = findViewById(R.id.btnReset)
+
+        startButton.setOnClickListener(View.OnClickListener {
+            if (!isRunning) {
+                startChronometer()
+                startButton.text = "Pausar"
+            } else {
+                pauseChronometer()
+                startButton.text = "Continuar"
+            }
+        })
+
+        resetButton.setOnClickListener(View.OnClickListener {
+            resetChronometer()
+            startButton.text = "Iniciar"
+        })
+    }
+
+    private fun startChronometer() {
+        handler.postDelayed(updateTimerThread, 0)
+        isRunning = true
+    }
+
+    private fun pauseChronometer() {
+        handler.removeCallbacks(updateTimerThread)
+        isRunning = false
+    }
+
+    private fun resetChronometer() {
+        handler.removeCallbacks(updateTimerThread)
+        isRunning = false
+        elapsedTime = 0L
+        chronometer.text = "00:00:00"
     }
 
 
+    private val updateTimerThread: Runnable = object : Runnable {
+        override fun run() {
+            elapsedTime += 1000
+            val hours = (elapsedTime / 3600000).toInt()
+            val minutes = ((elapsedTime - hours * 3600000) / 60000).toInt()
+            val seconds = ((elapsedTime - hours * 3600000 - minutes * 60000) / 1000).toInt()
+            chronometer.text = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+            handler.postDelayed(this, 1)
+            if(hours>99) elapsedTime=0;
+        }
+    }
 }
+
--- app/src/main/java/com/example/stopwatchandtimer/Timer.kt
+++ app/src/main/java/com/example/stopwatchandtimer/Timer.kt
@@ -1,19 +1,70 @@
 package com.example.stopwatchandtimer
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class Timer : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var btnReset: Button
+    private lateinit var btnStart: Button
+    private lateinit var btnGoToStopWatch: Button
+    private lateinit var countdownTimer: CountDownTimer
+    private lateinit var viewTempoTimer: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timer)
+        setContentView(R.layout.activity_timer) // Replace with your layout XML file
 
-        val btnGoToStopWatch = findViewById<Button>(R.id.btnGoToStopWatch);
-        btnGoToStopWatch.setOnClickListener{
-            val stopWatch = Intent(this@Timer, StopWatch::class.java)
-            startActivity(stopWatch)
+        editText = findViewById(R.id.editTextText)
+        btnReset = findViewById(R.id.btnReset)
+        btnStart = findViewById(R.id.btnStart)
+        btnGoToStopWatch = findViewById(R.id.btnGoToStopWatch)
+        viewTempoTimer = findViewById(R.id.viewTempoTimer);
+
+        btnReset.setOnClickListener { resetTimer() }
+        btnStart.setOnClickListener { startTimer() }
+        btnGoToStopWatch.setOnClickListener { navigateToStopWatch() }
+    }
+
+    private fun resetTimer() {
+        // Stop the countdown timer if it's running
+        if (::countdownTimer.isInitialized) {
+            countdownTimer.cancel()
         }
+        // Reset progress bar and EditText
+        viewTempoTimer.setText(getString(R.string.txt0))
+    }
+
+    private fun startTimer() {
+        val initialSeconds = editText.text.toString().toIntOrNull() ?: 0
+        val maxSeconds = 10 // Change this to your desired maximum seconds
+        resetTimer() // Reset the timer before starting a new one
+
+        countdownTimer = object : CountDownTimer((initialSeconds * 1000).toLong(), 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsRemaining = (millisUntilFinished / 1000).toInt()
+                viewTempoTimer.setText(secondsRemaining.toString())
+            }
+
+            override fun onFinish() {
+                // Handle timer completion here
+                viewTempoTimer.setText("Completo")
+            }
+        }
+
+        countdownTimer.start()
+    }
+
+    private fun navigateToStopWatch() {
+        val timer = Intent(this@Timer, StopWatch::class.java)
+        startActivity(timer)
     }
 }
